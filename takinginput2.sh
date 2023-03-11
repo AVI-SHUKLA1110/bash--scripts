494ae1a68580429874e3268285e052536cfd47e6
Abhishek Shukla <88367072+AVI-SHUKLA1110@users.noreply.github.com>
2023-03-11 20:39:36 +0530

    Add files via upload

---
+++ takinginput2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#here we see an extended form of taking multiple input from the user into a single line.
+
+echo "enter your age ,name and address"
+
+read name age  add 
+
+echo  "your name is ${name}, your age is ${age} , and your address is ${add}..."
