05da6591f6a52b8acdf9f5db56ceb6e642695d47
Abhishek Shukla <88367072+AVI-SHUKLA1110@users.noreply.github.com>
2023-03-11 20:27:44 +0530

    Add files via upload

---
+++ takinginput.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#here we are going to see how to take any input from the user in bash
+
+read -p  "enter your name :" name 
+read -p "enter your age: " age
+
+echo "my name is  ${name} & my age is ${age}"
